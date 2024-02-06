ffa77a68da29182f8e7aa8f0657b389e25e2be5d
SOURAV MONDAL <112825528+mondalsourav03242@users.noreply.github.com>
2024-02-06 22:57:37 +0530

    Add files via upload

---
+++ dash/data.js
@@ -0,0 +1,37 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'lorem ipsum dolar sit amet consectetur adipiscing alit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero',
+        image: 'images/slider 2.PNG'
+
+
+    },
+    {
+        name: 'loki',
+        des: 'lorem ipsum dolar sit amet consectetur adipiscing alit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero',
+        image: 'images/slider 1.PNG'
+
+
+    },
+    {
+        name: 'wanda vision',
+        des: 'lorem ipsum dolar sit amet consectetur adipiscing alit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero',
+        image: 'images/slider 3.PNG'
+
+
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'lorem ipsum dolar sit amet consectetur adipiscing alit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero',
+        image: 'images/slider 4.PNG'
+
+
+    },
+    {
+        name: 'luca',
+        des: 'lorem ipsum dolar sit amet consectetur adipiscing alit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero',
+        image: 'images/slider 5.PNG'
+
+
+    }
+]
